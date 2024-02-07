1c951f6c75963c96fca3ad46a94b6a9c628cd710
RawSalmon <phanthawasjira@Gmail.com>
2024-02-08 03:39:50 +0700

    Disabled unfinished components

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
           sharedBasicInfo={this.state.sharedData.basic_info}
         />
-        <Projects
+        {/* <Projects
           resumeProjects={this.state.resumeData.projects}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
@@ -132,7 +132,7 @@ class App extends Component {
         <Experience
           resumeExperience={this.state.resumeData.experience}
           resumeBasicInfo={this.state.resumeData.basic_info}
-        />
+        /> */}
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
       </div>
     );
